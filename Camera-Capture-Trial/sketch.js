3eea85aaf2d484474b41bd09640b48b67cec03f1
barrettwj <waylonbarrett6@gmail.com>
2016-06-07 20:10:03 -0500

    Update sketch.js

--- Camera-Capture-Trial/sketch.js
+++ Camera-Capture-Trial/sketch.js
@@ -1,11 +1,30 @@
 var capture;
 
 function setup() {
+  createCanvas(480, 120);
+  var constraints = {
+    video: {
+      mandatory: {
+        minWidth: 1280,
+        minHeight: 720
+      },
+      optional: [
+        { maxFrameRate: 10 }
+      ]
+    },
+    audio: true
+  };
+  createCapture(constraints, function(stream) {
+    //console.log(stream);
+  });
+}
+
+/*function setup() {
   createCanvas(1280, 720);
   capture = createCapture(VIDEO);
   capture.size(1280, 720);
   capture.hide();
-}
+}*/
 
 function draw() {
   image(capture, 0, 0);
