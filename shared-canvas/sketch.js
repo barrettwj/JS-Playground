8dda236d0002ebebc7ba47cc2273108ad661a794
barrettwj <waylonbarrett6@gmail.com>
2016-06-07 19:50:46 -0500

    Update sketch.js

--- shared-canvas/sketch.js
+++ shared-canvas/sketch.js
@@ -1,17 +1,22 @@
 var mouse_locs;
+var retrieved_mouse_locs;
 var max_length = 200;
 
 function setup() {
   createCanvas(600, 600);
-  mouse_locs = [];
+  mouse_locs = new Array();
+  retrieved_mouse_locs = new Array();
 }
 
 function draw() {
   background(0);
+  retrieved_mouse_locs = loadStrings('mouse_coords.txt');
+  //println(retrieved_mouse_locs.length);
   noStroke();
   fill(0, 255, 0, 200);
-  for (var i = 0; i < mouse_locs.length; i++) {
-    ellipse(mouse_locs[i].x, mouse_locs[i].y, 20, 20);
+  for (var i = 0; i < retrieved_mouse_locs.length; i++) {
+    var sa = split(retrieved_mouse_locs[i], ",");
+    ellipse(float(sa[0]), float(sa[1]), 20, 20);
   }
 }
 
@@ -19,6 +24,13 @@ function mouseDragged() {
   if (mouse_locs.length >= max_length) {
     mouse_locs.shift();
   }
-  mouse_locs.push(createVector(mouseX, mouseY));
+  var st = str(mouseX) + "," + str(mouseY);
+  append(mouse_locs, st);
+  return false;
+}
+
+function mouseReleased() {
+  saveStrings(mouse_locs, 'mouse_coords.txt');
+  //println("Saved!");
   return false;
 }
